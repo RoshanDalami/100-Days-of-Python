9ad5dc56172db580da023eda6a1b1019175d445f
roshan <roshan@roshan.roshan>
2022-04-11 20:39:02 +0530

    day18

--- day18/main.py
+++ day18/main.py
@@ -1,9 +1,21 @@
 from turtle import Turtle , Screen
-
+import random
 tur = Turtle()
-tur.shape("turtle")
+tur.pensize(5)
+colors = ["medium violet red","purple","dark magenta","violet","magenta","dark orchid"]
+def draw_shape(num_sides):
+    for _ in range(num_sides):
+        angle= 360 / num_sides
+        tur.forward(100)
+        tur.right(angle)
 
-
+for color in range(len(colors)):
+    tur.color(colors[color])
+for shape_side_n in range(3,11):
+    tur.color(random.choice(colors))
+    draw_shape(shape_side_n)
+    
+   
 
 
 
---
+++ day18/randomPath.py
@@ -0,0 +1,20 @@
+from turtle import Turtle , Screen
+import random
+tur = Turtle()
+randForward = random.random()*10
+randAngle = random.random()*360
+colors = ["medium violet red","purple","dark magenta","violet","magenta","dark orchid"]
+tur.pensize(10)
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
