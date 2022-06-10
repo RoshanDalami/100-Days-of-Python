55c57949d7e3919257563de554ffe43748f88927
rsunar <tech.channel.002@gmail.com>
2022-06-10 09:32:48 +0530

    day 27

--- day27/main.py
+++ day27/main.py
@@ -1,17 +1,32 @@
-import tkinter
+from tkinter import *
 
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My first GUI Program")
 window.minsize(width=500,height=500)
 
 
 #Label
 
-my_label = tkinter.Label(text="Hello , I am Roshan Dalami")
-my_label.pack()
+my_label = Label(text="Hello , I am Roshan Dalami",font=("Arial",24,"bold"))
+my_label.config(text="New ")
+my_label.grid(column=0,row=0)
 
 
+#Button
+def button_clicked():
+    user_input = input.get()
+    my_label.config(text=user_input)
+button = Button(text="click me" , command=button_clicked)
+button.grid(column=1,row=1)
+
+
+#Entry 
+input = Entry(width=10)
+input.grid(column=4,row=3)
+
+button1 = Button(text="Click Me 1")
+button1.grid(column=3,row=0)
 
 
 
